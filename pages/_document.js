6961a72f2e8a59f4bbc2dbb2735bd5915a9dab36
hacenselahy@gmail.com <hacenselahy@gmail.com>
2022-05-05 15:55:42 +0000

    adding code

--- pages/_document.js
+++ pages/_document.js
@@ -5,8 +5,9 @@ export default function Document() {
   return (
     <Html>
       <Head />
-      {/* <!-- Global site tag (gtag.js) - Google Analytics -->
-      <script
+
+      {/*  Global site tag (gtag.js) - Google Analytics */}
+      {/* <script
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-PNYH0CJWN6"
       ></script>
@@ -15,6 +16,24 @@ export default function Document() {
         {dataLayer.push(arguments)}
         gtag('js', new Date()); gtag('config', 'G-PNYH0CJWN6');
       </script> */}
+
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
 
       <body dir="rtl">
         <Main />
