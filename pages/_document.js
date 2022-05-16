7567b5538eb3b178341cc74417dcb674515a6fd6
hacenselahy@gmail.com <hacenselahy@gmail.com>
2022-05-16 12:52:22 +0000

    increase quality images

--- pages/_document.js
+++ pages/_document.js
@@ -7,46 +7,52 @@ export default function Document() {
       <Head />
 
       {/* the next pwa head  */}
-      <meta name="application-name" content="PWA App" />
+      <meta name="application-name" content="عقار نواكشوط" />
       <meta name="apple-mobile-web-app-capable" content="yes" />
       <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-      <meta name="apple-mobile-web-app-title" content="PWA App" />
-      <meta name="description" content="Best PWA App in the world" />
+      <meta
+        name="apple-mobile-web-app-title"
+        content="شركة للوساطة العقارية. ايجار و بيع الشقق و المنازل"
+      />
+      <meta
+        name="description"
+        content="شركة للوساطة العقارية. ايجار و بيع الشقق و المنازل"
+      />
       <meta name="format-detection" content="telephone=no" />
       <meta name="mobile-web-app-capable" content="yes" />
-      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      {/* <meta name="msapplication-config" content="/icons/browserconfig.xml" /> */}
       <meta name="msapplication-TileColor" content="#2B5797" />
       <meta name="msapplication-tap-highlight" content="no" />
       {/* <meta name="theme-color" content="#000000" /> */}
 
-      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link rel="apple-touch-icon" href="/icons/ios/icon-152.png" />
       <link
         rel="apple-touch-icon"
         sizes="152x152"
-        href="/icons/touch-icon-ipad.png"
+        href="/icons/ios/icon-152.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="180x180"
-        href="/icons/touch-icon-iphone-retina.png"
+        href="/icons/ios/icon-180.png"
       />
       <link
         rel="apple-touch-icon"
         sizes="167x167"
-        href="/icons/touch-icon-ipad-retina.png"
+        href="/icons/ios/icon-167.png"
       />
 
       <link
         rel="icon"
         type="image/png"
         sizes="32x32"
-        href="/icons/favicon-32x32.png"
+        href="/icons/ios/icon-32.png"
       />
       <link
         rel="icon"
         type="image/png"
         sizes="16x16"
-        href="/icons/favicon-16x16.png"
+        href="/icons/ios/icon-16.png"
       />
       <link rel="manifest" href="/manifest.json" />
       <link
@@ -61,22 +67,28 @@ export default function Document() {
       />
 
       <meta name="twitter:card" content="summary" />
-      <meta name="twitter:url" content="https://yourdomain.com" />
-      <meta name="twitter:title" content="PWA App" />
-      <meta name="twitter:description" content="Best PWA App in the world" />
+      <meta name="twitter:url" content="https://iqar.store" />
+      <meta name="twitter:title" content="عقار نواكشوط" />
+      <meta name="twitter:description" content="عقار نواكشوط" />
       <meta
         name="twitter:image"
         content="https://yourdomain.com/icons/android-chrome-192x192.png"
       />
-      <meta name="twitter:creator" content="@DavidWShadow" />
+      {/* <meta name="twitter:creator" content="@DavidWShadow" /> */}
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="PWA App" />
-      <meta property="og:description" content="Best PWA App in the world" />
-      <meta property="og:site_name" content="PWA App" />
-      <meta property="og:url" content="https://yourdomain.com" />
+      <meta
+        property="og:title"
+        content="عقار نواكشوط شركة للوساطة العقارية. ايجار و بيع الشقق و المنازل"
+      />
+      <meta
+        property="og:description"
+        content="عقار نواكشوط شركة للوساطة العقارية. ايجار و بيع الشقق و المنازل"
+      />
+      <meta property="og:site_name" content="عقار نواكشوط" />
+      <meta property="og:url" content="https://iqar.store" />
       <meta
         property="og:image"
-        content="https://yourdomain.com/icons/apple-touch-icon.png"
+        content="https://yourdomain.com/icons/ios/icon-152.png"
       />
 
       {/* The next pwa heads */}
